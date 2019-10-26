aa21898992a845f6625c26cc2f2f684bdcd3f148
aswinzz <iit2016106@iiita.ac.in>
2019-10-26 18:12:33 +0530

    add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Index from './pages/index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={};
+  }
+
+  render(){
+    return (
+      <div className="App">
+          <Switch>
+            <Route exact path="/" component={Index}/>
+          </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
